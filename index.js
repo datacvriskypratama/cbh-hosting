e2e1064ced3166dcbd681f5b0b5c6f999dc8ddd1
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-06-28 20:25:14 +0800

    All Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login("ODU5MDAyMjIzOTc4MzQ4NTQ0.YNmWKQ.CahWS3Wwt_o6sHvlsrV2CrHSzYA");
+client.on('ready', function () {
+    console.log("cbh-hosting");
+});
+
+client.on("message", function(msg) {
+    if(msg.channel.id == "859077598717018183" && msg.content === '/server') {
+        msg.reply('https://kotak-craft.herokuapp.com');
+    }
+});
+client.on("message", function(msg) {
+    if(msg.channel.id == "859077598717018183" && msg.content === '/dashboard') {
+        msg.reply('https://c4a58e04ae0e.ngrok.io/');
+    }
+});
+client.on("message", function(msg) {
+    if(msg.channel.id == "859077598717018183" && msg.content === '/join') {
+        msg.reply('Join server minecraft: kotak-craft.mcalias.com');
+    }
+});
