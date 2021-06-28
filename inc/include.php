c1b5c73bdab0a06f1f830419387fb96181d9604d
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-06-28 23:37:43 +0800

    All Commit

--- inc/include.php
+++ inc/include.php
@@ -1,11 +1,11 @@
 <?php 
 $server_name = "My server name";					//Name of your server
 $admin = "Admins name";									//Your name / IGN (just changes color in console for the admin name)
-$server_dir = "server\\";							//Directory of your Minecraft server files. 
+$server_dir = "server\\spigot-1.16.5.jar";							//Directory of your Minecraft server files. 
 $server_log = "logs\\latest.log";					//location of current server log inside server dir
 date_default_timezone_set('Europe/Amsterdam');		//Servers timezone
-$username = "username";								//Admin username (note: case sensitive)
-$password = "password";								//Admin password (note: case sensitive)
+$username = "akbar321";								//Admin username (note: case sensitive)
+$password = "akbar321";								//Admin password (note: case sensitive)
 $pmax = 200;										
 //pmax = Maximum number of last 'relevant' lines from the log it prints on the main webpage. So even if you dont restart your server for a long ass time and the log gets superbig, it wont take ages for the page to load.
 //PS, I've not put a limit to the serverlog.php, since as an admin you might want to see everything. Suppose the log does get superhuge, it might mean it will take too long to load, as such, that page also doesn't automatically refresh. If PHP times out, its time to restart your server. Cuz goddamn the log is huge then.
@@ -24,4 +24,3 @@ $omitarray = array	(
 					' [Server thread/WARN]:', 
 					'An unknown error occurred while attempting to perform this command'
 					);
-?>
